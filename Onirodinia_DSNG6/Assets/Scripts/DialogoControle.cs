5f6016005d3e5d2daf530e21e3c3264707a22d2a
Virginia Leticia <vlafonso@hotmail.com>
2021-11-25 10:19:29 -0300

    Add script controle dialogo

---
+++ Onirodinia_DSNG6/Assets/Scripts/DialogoControle.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogoControle : MonoBehaviour
+{
+    //Componentes
+    public GameObject objDialogo;
+    public Image personagem;
+    public Text txtFala;
+    public Text txtNomePersonagem;
+
+    //Configuracoes
+    public float tempoDigitacao;
+    private string[] sentenca;
+    private int index;
+//Funcao apresentar as falas
+    public void Fala(Sprite pers, string[] txt, string nome)
+    {
+        objDialogo.SetActive(true);
+        this.personagem.sprite = pers;
+        this.sentenca = txt;
+        this.txtNomePersonagem.text = nome;
+        StartCoroutine(DigitarSentenca());
+    }
+//Apresentar letra a letra no dialogo
+    IEnumerator DigitarSentenca()
+    {
+        foreach (char letras in this.sentenca[index].ToCharArray())
+        {
+            this.txtFala.text += letras;
+            yield return new WaitForSeconds(this.tempoDigitacao);
+        }
+    }
+//Pular para proxima sentenca
+    public void ProximaSentenca()
+    {
+        //Verifica se o txt esta completo
+        if(this.txtFala.text == this.sentenca[index])
+        {
+            //Verifica se ainda ha txt
+            if(index < this.sentenca.Length - 1)
+            {
+                index++;
+                this.txtFala.text = "";
+                StartCoroutine(DigitarSentenca());
+            }
+            //Quando acabar txt
+            else
+            {
+                PularDialogo();
+            }
+        }
+    }
+//Desativa dialogo e inicia fase 1
+    public void PularDialogo()
+    {
+        this.txtFala.text = "";
+        this.index = 0;
+        objDialogo.SetActive(false);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Fase_1");
+    }
+}
